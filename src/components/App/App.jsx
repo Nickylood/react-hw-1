7e89976a1fb19e0a5fe1a7de093eeceda1b54170
Dmytro Tkach <dreamworkshow@gmail.com>
2024-02-25 19:05:09 +0100

    нужно добавить useEffect

---
+++ src/components/App/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import data from "../json/data.json";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import ContactForm from "../ContactForm/ContactForm";
+
+export default function App() {
+  const [contacts, setContacts] = useState(data);
+  const [filter, setFilter] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts((prevContact) => {
+      return [...prevContact, newContact];
+    });
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts((prevContact) => {
+      return prevContact.filter((contact) => contact.id !== contactId);
+    });
+  };
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onAdd={addContact} />
+        <SearchBox value={filter} onFilter={setFilter} />
+        <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+      </div>
+    </>
+  );
+}
