5e900ca26a3ba6f018828203966056d19d384d27
Dmytro Tkach <dreamworkshow@gmail.com>
2024-02-29 12:34:09 +0100

    начало работы с контекстом 4 модуль последный раздел

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,11 @@
-import { useToggle } from "../../hooks/useToggle.js";
-import Modal from "../Modal.jsx";
+
 
 export default function App() {
-  const { isOpen, open, close } = useToggle(true);
+  
   return (
     <>
       <h1>Hello World</h1>
-      <button onClick={open}>Open modal</button>
-      <Modal isOpen={isOpen} onClose={close} />
+      
     </>
   );
 }
