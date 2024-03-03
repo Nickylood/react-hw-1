299832ff089b265bd1000e021a92847905499c74
Dmytro Tkach <dreamworkshow@gmail.com>
2024-03-03 17:23:37 +0100

    практика по 4 модулю

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,9 @@
-import Profile from "../Profile/Profile";
-import data from "../../json/userData.json";
-import TransactionHistory from "../TransactionHistory/TransactionHistory";
-// import FriendListItem from "../FriendListItem/FriendListItem";
-import transactions from '../../json/transactions.json'
 
-// import friends from '../../json/friends.json';
 
 export default function App() {
   return (
     <>
-      <Profile
-        name={data.username}
-        tag={data.tag}
-        location={data.location}
-        image={data.avatar}
-        stats={data.stats}
-      />
-      {/* <FriendListItem friends={friends} /> */}
-      <TransactionHistory items={transactions} />
+     
     </>
   );
 }
